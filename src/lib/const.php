737d2c59441baadbfedbe5b4d7a98532a8c1a8fa
André Jacques <andre.jacques@innoweb.ca>
2011-08-31 17:18:36 -0400

    add php const file

---
+++ src/lib/const.php
@@ -0,0 +1,10 @@
+<?php
+
+  define("EMPTY_STRING", "");
+  define("CR", "\r"); // For Mac
+  define("LF", "\n"); // For Linux
+  define("CRLF", CR . LF); // For Windows
+
+  define("ENTER", LF); // Your config
+
+?>
